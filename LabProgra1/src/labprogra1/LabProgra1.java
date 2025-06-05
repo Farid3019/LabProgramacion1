6227b3fd7bf353f5310f04d38885bbae13fa555f
Farid3019 <jfflores2005@gmail.com>
2025-06-05 17:14:11 -0600

    si

--- LabProgra1/src/labprogra1/LabProgra1.java
+++ LabProgra1/src/labprogra1/LabProgra1.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication4;
+package labprogra1;
 
 /**
  *
  * @author jfflo
  */
-public class JavaApplication4 {
+public class LabProgra1 {
 
     /**
      * @param args the command line arguments
